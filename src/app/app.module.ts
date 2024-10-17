55478a6dbdd515ed39a861db090f2a35b53709bd
AnandRaja-tech <anand.official.in@gmail.com>
2024-10-17 14:15:19 +0000

    basic reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
-import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './store/couter-store/counter.reducer';
 
 @NgModule({
   declarations: [AppComponent, CounterOutputComponent, CounterControlsComponent],
-  imports: [BrowserModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({
+      counter: counterReducer,
+      // auth: authReducer
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
