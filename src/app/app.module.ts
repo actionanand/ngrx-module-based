30361725c35588c4538a217b7011ce970845d984
AnandRaja-tech <anand.official.in@gmail.com>
2024-10-18 08:08:54 +0000

    ngrx effects completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 import { rootReducer } from './store/rootReducer';
-import { EffectsModule } from '@ngrx/effects';
+import { CounterEffects } from './store/couter-store/counter.effects';
 
 @NgModule({
   declarations: [AppComponent, CounterOutputComponent, CounterControlsComponent],
-  imports: [BrowserModule, StoreModule.forRoot(rootReducer), EffectsModule.forRoot([])],
+  imports: [BrowserModule, StoreModule.forRoot(rootReducer), EffectsModule.forRoot([CounterEffects])],
   providers: [],
   bootstrap: [AppComponent],
 })
